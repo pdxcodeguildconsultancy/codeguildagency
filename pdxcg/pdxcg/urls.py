13d6d98f2eaf828c67e34c4b526f55556382bd59
Christopher Jones <chris.charles.jones@gmail.com>
2014-07-08 23:23:56 -0700

    urls
    fixed issues with dup urls

--- pdxcg/pdxcg/urls.py
+++ pdxcg/pdxcg/urls.py
@@ -4,14 +4,11 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'pdxcg.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
+    (r'^accounts/logout/$', 'django.contrib.auth.views.logout',
+     {'next_page': '/'}),
     (r'^accounts/', include('allauth.urls')),
     url(r'^$', 'pdxcodeguild.views.index', name='mainpage'),
-    url(r'^admin/', include(admin.site.urls)),
     url(r'^about/$', 'pdxcodeguild.views.about', name='about'),
     url(r'^apply/$', 'pdxcodeguild.views.apply', name='apply'),
     url(r'^thanks/$', 'pdxcodeguild.views.thanks', name='thanks'),
