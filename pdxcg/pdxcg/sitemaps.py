7f74fe5491cba491ff91c035cd6e7ac9b91e3eee
Chris <chris.charles.jones@gmail.com>
2015-02-12 18:44:37 -0800

    fixed sitemap.py

--- pdxcg/pdxcg/sitemaps.py
+++ pdxcg/pdxcg/sitemaps.py
@@ -4,14 +4,28 @@ from django.core.urlresolvers import reverse
 
 from pdxcodeguild import views
 
+
 class StaticViewSitemap(sitemaps.Sitemap):
     priority = 0.5
     changefreq = 'daily'
-    #TODO fix urls in stie map
+
     def items(self):
-        return ['mainpage', 'about', 'apply', 'contact', 'gettechnical', 'individualized', 'jrdevbootcamp',
-                'evening_bootcamp', 'partner', 'program', 'sponsor', 'advisors', 'ppm', 'team',
-                ]
+        return [
+            'mainpage',
+            'about',
+            'apply',
+            'contact',
+            'gettechnical',
+            'devbootcamp',
+            'evening_bootcamp',
+            'partner',
+            'program',
+            'sponsor',
+            'advisors',
+            'ppm',
+            'team',
+            'students',
+            ]
 
     def location(self, item):
         return reverse(item)
